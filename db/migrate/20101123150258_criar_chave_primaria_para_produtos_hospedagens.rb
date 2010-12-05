3b04e0bad8d1384de982b394f1f7938be1aacc82
Dmitry L Rocha <dmitry.nix@gmail.com>
2010-12-05 16:46:01 -0300

    Corrigido migração

--- db/migrate/20101123150258_criar_chave_primaria_para_produtos_hospedagens.rb
+++ db/migrate/20101123150258_criar_chave_primaria_para_produtos_hospedagens.rb
@@ -1,9 +1,9 @@
 class CriarChavePrimariaParaProdutosHospedagens < ActiveRecord::Migration
   def self.up
-    add_column :produtos_hospedagens, :id, :serial
+    #add_column :produtos_hospedagens, :id, :serial
   end
 
   def self.down
-    remove_column :produtos_hospedagens, :id
+    #remove_column :produtos_hospedagens, :id
   end
 end
