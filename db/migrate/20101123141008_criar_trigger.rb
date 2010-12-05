fac9cc4cb4ff2ddfa3dfc6610558247e8f52c7a2
Dmitry L Rocha <dmitry.nix@gmail.com>
2010-12-05 16:44:28 -0300

    Removido migration de trigger para usar no heroku

--- db/migrate/20101123141008_criar_trigger.rb
+++ db/migrate/20101123141008_criar_trigger.rb
@@ -1,27 +1,27 @@
 class CriarTrigger < ActiveRecord::Migration
   def self.up
-    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      execute 'create language plpgsql;'
-      execute <<-END
-CREATE FUNCTION totaliza() RETURNS trigger AS
-$BODY$
+    #if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      #execute 'create language plpgsql;'
+      #execute <<-END
+#CREATE FUNCTION totaliza() RETURNS trigger AS
+#$BODY$
 
-begin
-  new.valor_total = new.valor * new.quantidade;
-  return new;
-end
-$BODY$
-  LANGUAGE "plpgsql"
-      END
-      execute 'create trigger tr_totaliza before insert or update on produtos_hospedagens for each row execute procedure totaliza();'
-    end
+#begin
+  #new.valor_total = new.valor * new.quantidade;
+  #return new;
+#end
+#$BODY$
+  #LANGUAGE "plpgsql"
+      #END
+      #execute 'create trigger tr_totaliza before insert or update on produtos_hospedagens for each row execute procedure totaliza();'
+    #end
   end
 
   def self.down
-    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      execute 'drop trigger tr_totaliza on produtos_hospedagens;'
-      execute 'drop function totaliza();'
-      execute 'drop language plpgsql;'
-    end
+    #if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      #execute 'drop trigger tr_totaliza on produtos_hospedagens;'
+      #execute 'drop function totaliza();'
+      #execute 'drop language plpgsql;'
+    #end
   end
 end
